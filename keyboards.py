b9000deb3dec2fe23c024fb6416c4750667a3ef1
Sharafiev ILnur <ilnursharafiev123@gmail.com>
2026-03-12 10:25:54 +0300

    кнопки для меню

--- keyboards.py
+++ keyboards.py
@@ -1,2 +1,9 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='Как это работает?')],
+        [KeyboardButton(text='Избранное')]
+    ],
+    resize_keyboard=True#не широка клава
+)
