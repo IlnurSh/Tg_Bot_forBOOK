1b92b8bac0a3f418606e502648089aea41169b4d
Sharafiev ILnur <ilnursharafiev123@gmail.com>
2026-03-12 10:37:40 +0300

    обработчик кнопки как работает

--- handlers.py
+++ handlers.py
@@ -3,8 +3,35 @@ from aiogram.fsm.context import FSMContext #fsm нужен чтобы цправ
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart
 
+import keyboards
+
+
 user = Router()
 
-@user.message()
-async def echo(message:Message):
-    await message.send_copy(chat_id=message.from_user.id)
+# @user.message()
+# async def echo(message:Message):
+#     await message.send_copy(chat_id=message.from_user.id) эхо
+
+
+@user.message(CommandStart())
+async def cmd_start(message:Message):
+    await message.answer("Добро пожаловать в BookWiseBot", reply_markup=keyboards.menu)
+
+@user.message(F.text == 'Как это работает?')
+async def cmd_help(message:Message):
+    text = """
+    📚 <b>Как работает BookWiseBot:</b>
+
+    1. Ты присылаешь любую цитату из книги
+    2. Бот отправляет её в нейросеть GigaChat
+    3. GigaChat определяет:
+    • Название книги
+    • Автора
+    • Смысл цитаты
+    4. Бот показывает результат и предлагает:
+    • Сохранить в избранное ❤️
+    • Найти похожие книги 📖
+
+    Просто отправь цитату и увидишь магию! ✨
+        """
+    await message.answer(text,parse_mode='HTML')
